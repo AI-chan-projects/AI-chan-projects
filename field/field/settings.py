5a014a5a821d854ab3c796ab7453e8cac01e37bc
LeeYoungChan <youngchan.lee.ai@gmail.com>
2023-12-05 10:28:18 +0900

    Django digitrecognizer served

--- field/field/settings.py
+++ field/field/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--$j&#b%7oeyv!q24*3^nn^i&*0p!ik!23-kud&#@_1i(96+2te"
+SECRET_KEY = "나만알지롱"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [''] # 외부접속시 아이피 기재 후 RUNSERVER IPADDRESS:PORT
 
 
 # Application definition
