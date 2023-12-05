43d673f50d037ba07dfde5719fd4dd8539df3b6c
LeeYoungChan <youngchan.lee.ai@gmail.com>
2023-12-05 10:42:16 +0900

    apply .gitignore

--- field/field/settings.py
+++ field/field/settings.py
@@ -11,16 +11,29 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 
+# SECURITY WARNING: keep the secret key used in production secret!
+secret_file = os.path.join("/", 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "나만알지롱"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
